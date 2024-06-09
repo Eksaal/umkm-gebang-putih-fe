7c760e51b2fd2957df5e9170a482a07e1e6a1350
Agus Darmawan <5024211070@mhs.its.ac.id>
2024-06-09 07:47:48 +0700

    fix: unusued console

--- hooks/useUMKM.ts
+++ hooks/useUMKM.ts
@@ -4,7 +4,6 @@ export const useUmkm = () => {
     const getMetaUmkm = async () => {
         try {
             const response = await axios.get('/umkm');
-            console.log(response.data.data);
             return response.data.data;
         } catch (error) {
             console.error('Failed to store data', error);
