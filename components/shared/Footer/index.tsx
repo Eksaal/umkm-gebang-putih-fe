f0d4bf3a3795cd1720f4da53ea6bfd19310beedc
Eksaal <ekasalmasalsabilla@gmail.com>
2024-06-10 20:49:49 +0700

    commit

--- components/shared/Footer/index.tsx
+++ components/shared/Footer/index.tsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import { FaPhone, FaMapMarkerAlt, FaEnvelope } from 'react-icons/fa'
-import { FaMessage } from "react-icons/fa6";
+import { FaMessage } from "react-icons/fa6"
 import Image from 'next/image'
+import Link from 'next/link';
 
 const contacts = [
     {
         icon: <FaMapMarkerAlt />,
+        href: '/https://www.google.com/maps/place/Gebang+Putih,+Kec.+Sukolilo,+Surabaya,+Jawa+Timur/@-7.2819328,112.7867909,16z/data=!3m1!4b1!4m6!3m5!1s0x2dd7fa1487ff2801:0x790b0bf6776f8da2!8m2!3d-7.2833898!4d112.788207!16s%2Fg%2F121bk7y3?entry=ttu',
         text: 'Jalan Raya Gebang Putih, Surabaya',
     },
     {
         icon: <FaPhone />,
+        href: '/https://wa.me/6282250907901',
         text: '+62 822 5090 7901',
     },
     {
         icon: <FaEnvelope />,
+        href: 'https://mail.google.com/mail/u/0/?view=cm&tf=1&fs=1&to=umkmgebangid@gmail.com',
         text: 'umkmgebangid@gmail.com',
     },
     {
         icon: <FaMessage />,
+        href: 'https://docs.google.com/forms/d/e/1FAIpQLSfp3l9nSwdosAWtO4qshtqsEDoyuU1zOvsmoWbPSoDRc11LvA/viewform',
         text: 'https://its.id/m/SurveiKepuasanUMKMGebang',
     }
 ]
@@ -42,10 +47,12 @@ export default function Footer() {
                 <ul className="list-unstyled space-y-2">
                     {contacts.map((contact, index) => (
                         <li key={index} className="flex items-center gap-2">
+                            <Link href={contact.href}>
                             <span className="text-green-600">
                                 {contact.icon}
                             </span>
                             <span>{contact.text}</span>
+                            </Link>
                         </li>
                     ))}
                 </ul>
