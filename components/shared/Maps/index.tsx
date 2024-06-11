0f8872bd00afacf09eeda42859c7602a42b9fa23
Eksaal <ekasalmasalsabilla@gmail.com>
2024-06-11 10:55:31 +0700

    maps

--- components/shared/Maps/index.tsx
+++ components/shared/Maps/index.tsx
@@ -52,7 +52,7 @@ const Map: React.FC<MapProps> = ({ locations }) => {
     return (
         <MapContainer
             center={[-7.282862, 112.785852]}
-            zoom={30}
+            zoom={100}
             className="fixed left-0 right-0 h-full w-full"
         >
             <TileLayer
