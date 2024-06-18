de1092b17f473f8a5010be1e44dccb350c8a9f9a
Eksaal <ekasalmasalsabilla@gmail.com>
2024-06-18 12:16:31 +0700

    maps

--- components/shared/Maps/index.tsx
+++ components/shared/Maps/index.tsx
@@ -50,31 +50,52 @@ const Map: React.FC<MapProps> = ({ locations }) => {
     }
 
     return (
-        <MapContainer
-            center={[-7.282862, 112.785852]}
-            zoom={100}
-            className="fixed left-0 right-0 h-full w-full"
-        >
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            />
-            {filteredLocations.map((location) => (
-                <Marker
-                    key={location.id}
-                    icon={createCustomIcon(
-                        location.category === 'Makanan' ? 'red' : 'green',
-                        location.name,
-                    )}
-                    position={[location.latitude, location.longitude]}
-                    eventHandlers={{
-                        click: () => handleMarkerClick(location.name),
-                    }}
+        <div className="flex flex-col h-screen w-full">
+            <div className="flex flex-col md:flex-row items-center justify-between p-4 bg-white shadow-md">
+                <input
+                    type="text"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    placeholder="Cari UMKM..."
+                    className="w-full md:w-1/3 mb-2 md:mb-0 p-2 border border-gray-300 rounded-md"
+                />
+                <select
+                    value={selectedType}
+                    onChange={(e) => setSelectedType(e.target.value)}
+                    className="w-full md:w-1/3 p-2 border border-gray-300 rounded-md"
                 >
-                    <Popup>{location.name}</Popup>
-                </Marker>
-            ))}
-        </MapContainer>
+                    <option value="All">Semua Kategori</option>
+                    <option value="Makanan">Makanan</option>
+                    <option value="Minuman">Minuman</option>
+                    <option value="Lainnya">Lainnya</option>
+                </select>
+            </div>
+            <MapContainer
+                center={[-7.282862, 112.785852]}
+                zoom={13}
+                className="flex-1"
+            >
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                {filteredLocations.map((location) => (
+                    <Marker
+                        key={location.id}
+                        icon={createCustomIcon(
+                            location.category === 'Makanan' ? 'red' : 'green',
+                            location.name,
+                        )}
+                        position={[location.latitude, location.longitude]}
+                        eventHandlers={{
+                            click: () => handleMarkerClick(location.name),
+                        }}
+                    >
+                        <Popup>{location.name}</Popup>
+                    </Marker>
+                ))}
+            </MapContainer>
+        </div>
     )
 }
 
