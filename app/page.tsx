925d32e5b9a6bf1547f94e0e6787e4046c840ecf
Eksaal <ekasalmasalsabilla@gmail.com>
2024-06-18 10:11:51 +0700

    w

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,6 @@ const HomePage: React.FunctionComponent = () => {
                     alt="hero-image"
                     height={240}
                     width={1180}
-                    className="w-full"
                 />
                 <div className="absolute left-4 top-20 sm:left-10 sm:top-40 text-white">
                     <h2 className="text-3xl sm:text-5xl font-extrabold">
